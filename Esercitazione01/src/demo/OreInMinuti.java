035d8f40a20ca10bb58ddf7e4405ac0c7a17e264
anSavarino <153192723+anSavarino@users.noreply.github.com>
2023-12-18 18:31:20 +0100

    Add files via upload

--- Esercitazione01/src/demo/OreInMinuti.java
+++ Esercitazione01/src/demo/OreInMinuti.java
@@ -1,10 +1,21 @@
 package demo;
 
+import java.util.Scanner;
+
 public class OreInMinuti {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero frazionario che rappresenta un tempo espresso in ore: ");
+		double numFrazionario = scanner.nextDouble();
+		
+		double minuti = numFrazionario * 60;
+		
+		System.out.println((int)minuti);
+		
+		
 	}
 
 }
