7df895f15c427449a2164e757501e874ed8f6842
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2023-12-18 18:08:32 +0100

    Push MinutiinOre

--- Esercitazione01/src/demo/MinutiInOre.java
+++ Esercitazione01/src/demo/MinutiInOre.java
@@ -1,10 +1,22 @@
 package demo;
 
+import java.util.Scanner;
+
 public class MinutiInOre {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero numero di minuti: ");
+		int minuti = scanner.nextInt();
+		
+		int ore = minuti / 60;
+		int minutiResto = minuti % 60;
+		
+		System.out.println(minuti+" minuti equivalgono a "+ore+" ore e "+minutiResto+" minuti");
+		
+		
 	}
 
 }
