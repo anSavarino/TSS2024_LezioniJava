8bf84df5e5771ab62580a9b83e5308d7820c2856
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-02-28 13:45:07 +0100

    28/02 2
    prova

---
+++ Prj28_IMDBFilm2/src/main/java/model/Film.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Film implements Comparable<Film>{
+	
+	private int id;
+	private double rating;
+	private String titolo;
+	private int anno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public int getAnno() {
+		return anno;
+	}
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+	@Override
+	public String toString() {
+		return "Film [id=" + id + ", rating=" + rating + ", titolo=" + titolo + ", anno=" + anno + "]";
+	}
+	@Override
+	public int compareTo(Film o) {
+		return this.anno - o.anno;
+	}
+	
+	
+	
+}
